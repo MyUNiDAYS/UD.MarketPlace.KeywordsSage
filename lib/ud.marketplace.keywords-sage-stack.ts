ef44b32d7dfce5d2e15884f15a496b38dc96691d
Rubem Moreira <rubismoreira@gmail.com>
2025-05-07 17:59:58 +0200

    fix cdk

--- lib/ud.marketplace.keywords-sage-stack.ts
+++ lib/ud.marketplace.keywords-sage-stack.ts
@@ -12,206 +12,223 @@ import {EnvVariablesSchema, getProcessEnvVariables} from "./helpers/types";
 import {generateResourcename} from "./helpers/generateResourceName";
 
 export class UdMarketplaceKeywordsSageStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, {
-      ...props,
-      env: {
-        account: process.env.CDK_DEFAULT_ACCOUNT,
-        region: process.env.CDK_DEFAULT_REGION,
-      },
-    });
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, {
+            ...props,
+            env: {
+                account: process.env.CDK_DEFAULT_ACCOUNT,
+                region: process.env.CDK_DEFAULT_REGION,
+            },
+        });
 
-    const { ENV } = getProcessEnvVariables(EnvVariablesSchema);
+        const { ENV } = getProcessEnvVariables(EnvVariablesSchema);
 
-    const tags: Record<string, string> = {
-      "ud:domain": "UD.Marketplace",
-      "ud:system": "Keywords-Sage",
-      "ud:maintainers": "web.team@myunidays.com",
-      "ud:git-repo": "https://github.com/MyUNiDAYS/UD.MarketPlace.KeywordsSage",
-      "ud:env": ENV,
-    };
+        const tags: Record<string, string> = {
+            "ud:domain": "UD.Marketplace",
+            "ud:system": "Keywords-Sage",
+            "ud:maintainers": "web.team@myunidays.com",
+            "ud:git-repo": "https://github.com/MyUNiDAYS/UD.MarketPlace.KeywordsSage",
+            "ud:env": ENV,
+        };
 
-    Object.keys(tags).forEach((key) => {
-      cdk.Tags.of(this).add(key, tags[key]);
-    });
+        Object.keys(tags).forEach((key) => {
+            cdk.Tags.of(this).add(key, tags[key]);
+        });
 
-    const keyWordsSageTaskDef = new ecs.TaskDefinition(
-      this,
-      "KeywodsSageTaskDef",
-      {
-        memoryMiB: "512",
-        cpu: "256",
-        compatibility: ecs.Compatibility.EC2,
-        networkMode: ecs.NetworkMode.BRIDGE,
-        family: generateResourcename("KeywodsSageTaskDef"),
-      }
-    );
+        // Create a task execution role with Bedrock permissions
+        const keywordsSageTaskRole = new iam.Role(this, 'KeywordsSageTaskRole', {
+            assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
+            roleName: generateResourcename('KeywordsSageTaskRole'),
+            description: 'Role for KeywordsSage ECS task with Bedrock access',
+        });
 
-      keyWordsSageTaskDef.addContainer("KewordsSageContainer", {
-      image: ecs.ContainerImage.fromAsset(path.join(__dirname, "../"), {
-        file: "Dockerfile",
-      }),
-      logging: new ecs.AwsLogDriver({
-        streamPrefix: "keywordsSageContainer",
-      }),
-      portMappings: [
-        {
-          containerPort: 3000,
-          protocol: ecs.Protocol.TCP,
-        },
-      ],
-      containerName: "keywordsSageContainer",
-    });
+        // Add Bedrock permissions
+        keywordsSageTaskRole.addToPolicy(new iam.PolicyStatement({
+            actions: [
+                'bedrock:InvokeModel',
+                'bedrock:InvokeModelWithResponseStream'
+            ],
+            resources: ['*'], // For more restrictive permissions, you can specify the model ARN
+        }));
 
-    const vpcId = ENV == "dev" ? "vpc-5a64083c" : "vpc-ed997485";
+        const keyWordsSageTaskDef = new ecs.TaskDefinition(
+            this,
+            "KeywordsSageTaskDef",
+            {
+                memoryMiB: "512",
+                cpu: "256",
+                compatibility: ecs.Compatibility.EC2,
+                networkMode: ecs.NetworkMode.BRIDGE,
+                family: generateResourcename("KeywordsSageTaskDef"),
+                taskRole: keywordsSageTaskRole,
+            }
+        );
 
-    new cdk.CfnOutput(this, "VpcIdOutput", {
-      value: vpcId,
-      description: "The imported VPC ID",
-    });
-
-    const vpc = ec2.Vpc.fromLookup(this, "VPC", {
-      vpcId,
-    });
-
-    const keywordsSageTaskTargetGroup = new elbv2.ApplicationTargetGroup(
-      this,
-      "KeywordsSageTaskTargetGroup",
-      {
-        vpc,
-        port: 3000,
-        protocol: elbv2.ApplicationProtocol.HTTP,
-        targetType: elbv2.TargetType.INSTANCE,
-        healthCheck: {
-          path: "/health",
-          timeout: cdk.Duration.seconds(5),
-          interval: cdk.Duration.seconds(30),
-          healthyThresholdCount: 5,
-          unhealthyThresholdCount: 2,
-        },
-        targetGroupName: "KeywordsSageTaskTargetGroup",
-      }
-    );
-
-    keywordsSageTaskTargetGroup.setAttribute(
-      "load_balancing.algorithm.type",
-      "round_robin"
-    );
-
-    const clusterImport = cdk.Fn.importValue("Services-ECS-Cluster");
-    const capacityProviderImport = cdk.Fn.importValue(
-      "Services-ECS-InternetCapacityProvider"
-    );
-
-    const httpsListenerArn =
-      ENV === "dev"
-        ? "arn:aws:elasticloadbalancing:eu-west-1:544207747350:listener/app/APIServices-External/80acd573b18e6a4d/124b0e333064ee4d"
-        : "arn:aws:elasticloadbalancing:eu-west-1:397592739218:listener/app/APIServices-External/746f127a6e2c32d8/d3824c49fa7fa88a";
-
-    const hostHeaders =
-      ENV === "dev"
-        ? "keywods-sage.dev.unidays.io"
-        : "keywords-sage.prod.unidays.io";
-
-    const lbFullName =
-      ENV === "dev"
-        ? "app/APIServices-External/80acd573b18e6a4d"
-        : "app/APIServices-External/746f127a6e2c32d8";
-
-    new elbv2.ApplicationListenerRule(this, (id = "KeywordsSageListenerRule"), {
-      listener: elbv2.ApplicationListener.fromLookup(this, "Listener", {
-        listenerArn: httpsListenerArn,
-      }),
-      priority: 205,
-      conditions: [
-        elbv2.ListenerCondition.httpRequestMethods(["POST"]),
-        elbv2.ListenerCondition.pathPatterns(["/generate-keywords"]),
-        elbv2.ListenerCondition.hostHeaders([hostHeaders]),
-      ],
-      action: elbv2.ListenerAction.forward([keywordsSageTaskTargetGroup]),
-    });
-
-    const keywordsSageService = new ecs.CfnService(
-      this,
-      "KeywordsSageService",
-      {
-        cluster: clusterImport,
-        capacityProviderStrategy: [
-          {
-            capacityProvider: capacityProviderImport,
-            weight: 1,
-          },
-        ],
-        desiredCount: 1,
-        healthCheckGracePeriodSeconds: 30,
-        loadBalancers: [
-          {
+        keyWordsSageTaskDef.addContainer("KeywordsSageContainer", {
+            image: ecs.ContainerImage.fromAsset(path.join(__dirname, "../"), {
+                file: "Dockerfile",
+            }),
+            logging: new ecs.AwsLogDriver({
+                streamPrefix: "keywordsSageContainer",
+            }),
+            portMappings: [
+                {
+                    containerPort: 3000,
+                    protocol: ecs.Protocol.TCP,
+                },
+            ],
             containerName: "keywordsSageContainer",
-            containerPort: 3000,
-            targetGroupArn: keywordsSageTaskTargetGroup.targetGroupArn,
-          },
-        ],
-        taskDefinition: keyWordsSageTaskDef.taskDefinitionArn,
-        enableEcsManagedTags: true,
-        propagateTags: "TASK_DEFINITION",
-        placementStrategies: [
-          {
-            field: "attribute:ecs.availability-zone",
-            type: "spread",
-          },
-          {
-            field: "instanceId",
-            type: "spread",
-          },
-        ],
-        tags: [
-          {
-            key: "ud:owner",
-            value: "WebTeam",
-          },
-        ],
-      }
-    );
+        });
 
-    const scalableTarget = new applicationautoscaling.ScalableTarget(
-      this,
-      "KeywordsSageScalability",
-      {
-        serviceNamespace: applicationautoscaling.ServiceNamespace.ECS,
-        maxCapacity: 2,
-        minCapacity: 1,
-        resourceId: `service/${clusterImport}/${keywordsSageService.attrName}`,
-        scalableDimension: "ecs:service:DesiredCount",
-      }
-    );
+        const vpcId = ENV == "dev" ? "vpc-5a64083c" : "vpc-ed997485";
 
-    scalableTarget.addToRolePolicy(
-      new iam.PolicyStatement({
-        actions: [
-          "ecs:UpdateService",
-          "ecs:DescribeServices",
-          "ecs:DescribeClusters",
-          "application-autoscaling:*",
-          "cloudwatch:DescribeAlarms",
-          "cloudwatch:GetMetricStatistics",
-        ],
-        resources: [`*`],
-      })
-    );
+        new cdk.CfnOutput(this, "VpcIdOutput", {
+            value: vpcId,
+            description: "The imported VPC ID",
+        });
 
-    new applicationautoscaling.TargetTrackingScalingPolicy(
-      this,
-      "KeywordsSageTargetTrackingPolicy",
-      {
-        policyName: "KeywordsSageTargetTrackingPolicy",
-        scalingTarget: scalableTarget,
-        targetValue: 200,
-        predefinedMetric:
-          applicationautoscaling.PredefinedMetric.ALB_REQUEST_COUNT_PER_TARGET,
-        resourceLabel: `${lbFullName}/${keywordsSageTaskTargetGroup.targetGroupFullName}`,
-        scaleInCooldown: cdk.Duration.seconds(60),
-        scaleOutCooldown: cdk.Duration.seconds(60),
-      }
-    );
-  }
+        const vpc = ec2.Vpc.fromLookup(this, "VPC", {
+            vpcId,
+        });
+
+        const keywordsSageTaskTargetGroup = new elbv2.ApplicationTargetGroup(
+            this,
+            "KeywordsSageTaskTargetGroup",
+            {
+                vpc,
+                port: 3000,
+                protocol: elbv2.ApplicationProtocol.HTTP,
+                targetType: elbv2.TargetType.INSTANCE,
+                healthCheck: {
+                    path: "/health",
+                    timeout: cdk.Duration.seconds(5),
+                    interval: cdk.Duration.seconds(30),
+                    healthyThresholdCount: 5,
+                    unhealthyThresholdCount: 2,
+                },
+                targetGroupName: "KeywordsSageTaskTargetGroup",
+            }
+        );
+
+        keywordsSageTaskTargetGroup.setAttribute(
+            "load_balancing.algorithm.type",
+            "round_robin"
+        );
+
+        const clusterImport = cdk.Fn.importValue("Services-ECS-Cluster");
+        const capacityProviderImport = cdk.Fn.importValue(
+            "Services-ECS-InternetCapacityProvider"
+        );
+
+        const httpsListenerArn =
+            ENV === "dev"
+                ? "arn:aws:elasticloadbalancing:eu-west-1:544207747350:listener/app/APIServices-External/80acd573b18e6a4d/124b0e333064ee4d"
+                : "arn:aws:elasticloadbalancing:eu-west-1:397592739218:listener/app/APIServices-External/746f127a6e2c32d8/d3824c49fa7fa88a";
+
+        const hostHeaders =
+            ENV === "dev"
+                ? "keywords-sage.dev.unidays.io"
+                : "keywords-sage.prod.unidays.io";
+
+        const lbFullName =
+            ENV === "dev"
+                ? "app/APIServices-External/80acd573b18e6a4d"
+                : "app/APIServices-External/746f127a6e2c32d8";
+
+        new elbv2.ApplicationListenerRule(this, "KeywordsSageListenerRule", {
+            listener: elbv2.ApplicationListener.fromLookup(this, "Listener", {
+                listenerArn: httpsListenerArn,
+            }),
+            priority: 205,
+            conditions: [
+                elbv2.ListenerCondition.httpRequestMethods(["POST"]),
+                elbv2.ListenerCondition.pathPatterns(["/generate-keywords"]),
+                elbv2.ListenerCondition.hostHeaders([hostHeaders]),
+            ],
+            action: elbv2.ListenerAction.forward([keywordsSageTaskTargetGroup]),
+        });
+
+        const keywordsSageService = new ecs.CfnService(
+            this,
+            "KeywordsSageService",
+            {
+                cluster: clusterImport,
+                capacityProviderStrategy: [
+                    {
+                        capacityProvider: capacityProviderImport,
+                        weight: 1,
+                    },
+                ],
+                desiredCount: 1,
+                healthCheckGracePeriodSeconds: 30,
+                loadBalancers: [
+                    {
+                        containerName: "keywordsSageContainer",
+                        containerPort: 3000,
+                        targetGroupArn: keywordsSageTaskTargetGroup.targetGroupArn,
+                    },
+                ],
+                taskDefinition: keyWordsSageTaskDef.taskDefinitionArn,
+                enableEcsManagedTags: true,
+                propagateTags: "TASK_DEFINITION",
+                placementStrategies: [
+                    {
+                        field: "attribute:ecs.availability-zone",
+                        type: "spread",
+                    },
+                    {
+                        field: "instanceId",
+                        type: "spread",
+                    },
+                ],
+                tags: [
+                    {
+                        key: "ud:owner",
+                        value: "WebTeam",
+                    },
+                ],
+            }
+        );
+
+        const scalableTarget = new applicationautoscaling.ScalableTarget(
+            this,
+            "KeywordsSageScalability",
+            {
+                serviceNamespace: applicationautoscaling.ServiceNamespace.ECS,
+                maxCapacity: 2,
+                minCapacity: 1,
+                resourceId: `service/${clusterImport}/${keywordsSageService.attrName}`,
+                scalableDimension: "ecs:service:DesiredCount",
+            }
+        );
+
+        scalableTarget.addToRolePolicy(
+            new iam.PolicyStatement({
+                actions: [
+                    "ecs:UpdateService",
+                    "ecs:DescribeServices",
+                    "ecs:DescribeClusters",
+                    "application-autoscaling:*",
+                    "cloudwatch:DescribeAlarms",
+                    "cloudwatch:GetMetricStatistics",
+                ],
+                resources: [`*`],
+            })
+        );
+
+        new applicationautoscaling.TargetTrackingScalingPolicy(
+            this,
+            "KeywordsSageTargetTrackingPolicy",
+            {
+                policyName: "KeywordsSageTargetTrackingPolicy",
+                scalingTarget: scalableTarget,
+                targetValue: 200,
+                predefinedMetric:
+                applicationautoscaling.PredefinedMetric.ALB_REQUEST_COUNT_PER_TARGET,
+                resourceLabel: `${lbFullName}/${keywordsSageTaskTargetGroup.targetGroupFullName}`,
+                scaleInCooldown: cdk.Duration.seconds(60),
+                scaleOutCooldown: cdk.Duration.seconds(60),
+            }
+        );
+    }
 }
