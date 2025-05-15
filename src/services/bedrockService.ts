6d30c34b147baee523f078cdc5354e250e36e317
Rubem Moreira <rubismoreira@gmail.com>
2025-05-15 17:50:51 +0200

    update command

--- src/services/bedrockService.ts
+++ src/services/bedrockService.ts
@@ -7,6 +7,7 @@ import * as path from "path";
 
 const bedrockClient = new BedrockRuntimeClient({
   region: "eu-west-1",
+  profile: "unidays-dev",
 });
 
 const claudeRequestTemplate = fs.readFileSync(
@@ -58,10 +59,10 @@ function generateKeywordPrompt(
   initialKeywords: string[]
 ): string {
   return `I have a discount platform with partners that can go on different categories: fashion; food & drink; technology; beauty; travel & lifestyle; wellbeing; health & fitness. 
-            Generate from 5 to 20 common UK-specific search terms that university students would use when looking for items that this partner may have for these students. 
-            I dont want the partner name on its keywords and try to keep these keywords to single keywords. These should be general and specific product keywords that I could direct to the following brand: ${partner}.
+            Recommend from 10 to 20 suggestions of common UK-specific search terms that university students would use when looking for items that this partner may have for them. 
+            ${initialKeywords.length > 0 ? `In order to help you some initial keywords marketing team provided some initial keywords related to this partner: ${initialKeywords.join(", ")}. ` : ""}
+            I don't want the partner name on its keywords and try to keep these keywords to single keywords. These should be general and specific product keywords that I could direct to the following brand: ${partner}.
             So for instance I would like to iphone or ipad to redirect me to Apple. Or galaxy to redirect me to Samsung. 
             Use proper British terminology. Please avoid composite words into one single word such as galaxybook.
-            ${initialKeywords.length > 0 ? `In order to help you some initial keywords i tought for this partner are: ${initialKeywords.join(", ")}. ` : ""}
-            I want the response to be as less verbose as possible on json format [{ "keywords" : ["keyword1", "keyword2"...]}] `;
+            The response should simply contain a single json in the following format: { "keywords" : ["keyword1", "keyword2"...]} where keyword1, keyword2 are the keywords you suggest.`;
 }
